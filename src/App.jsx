ba04a37085afa165dd5ef6a109058bcf150ed043
LachezarBogdanov <lachezarbogdanov22@gmail.com>
2025-02-26 19:16:01 +0200

    Added cart page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import './App.css'
 import Footer from './components/Home/Footer/Footer';
 import Header from './components/Home/Header/Header';
 
-import Create from './components/Home/Create/Create';
+import Cart from './components/Home/Cart/Cart';
 
 function App() {
 
   return (
     <>
     <Header />
-    <Create />
+    <Cart />
     <Footer />
     </>
   )
