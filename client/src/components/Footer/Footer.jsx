2b514b4ec4c3e480e49c742e2803196399856f73
LachezarBogdanov <lachezarbogdanov22@gmail.com>
2025-04-01 19:34:11 +0300

    Add media for shop and home

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,43 +5,47 @@ export default function Footer() {
         <footer>
         <h1>WHEYPRO</h1>
         <div className={styles.footerWrapper}>
-        <section className={styles.address}>
-        <h2>
-        <i className="fa-solid fa-location-dot" />
-        Address
-        </h2>
-        <p>Sofia, bul.Tsarigradsko 160</p>
-        </section>
-        <section className={styles.mobile}>
-        <h2>
-        <i className="fa-solid fa-phone" />
-        Phone-number
-        </h2>
-        <p>0888888888</p>
-        </section>
-        <section className={styles.email}>
-        <h2>
-        <i className="fa-solid fa-envelope" />
-        E-mail
-        </h2>
-        <p>wheypro-support@gmail.com</p>
-        </section>
+            <section className={styles.address}>
+                <h2>
+                    <i className="fa-solid fa-location-dot" />
+                    Address
+                </h2>
+                <p>Sofia, bul.Tsarigradsko 160</p>
+            </section>
+
+            <section className={styles.mobile}>
+                <h2>
+                    <i className="fa-solid fa-phone" />
+                    Phone-number
+                </h2>
+                <p>0888888888</p>
+            </section>
+
+            <section className={styles.email}>
+                <h2>
+                    <i className="fa-solid fa-envelope" />
+                    E-mail
+                </h2>
+                <p>wheypro-support@gmail.com</p>
+            </section>
         </div>
+
         <div className={styles.icons}>
-        <div className={styles.iconsWrapper}>
-        <a href="#">
-        <i className="fa-brands fa-facebook" />
-        </a>
-        <a href="#">
-        <i className="fa-brands fa-instagram" />
-        </a>
-        <a href="#">
-        <i className="fa-brands fa-twitter" />
-        </a>
+            <div className={styles.iconsWrapper}>
+                <a href="#">
+                    <i className="fa-brands fa-facebook" />
+                </a>
+                <a href="#">
+                    <i className="fa-brands fa-instagram" />
+                </a>
+                <a href="#">
+                    <i className="fa-brands fa-twitter" />
+                </a>
+            </div>
+            <p>© All rights reserved 23.02.2025</p>
         </div>
-        <p>© All rights reserved 23.02.2025</p>
-        </div>
-        </footer>
+</footer>
+
         
     )
 }
